8faf1dfbc63944deb320f2b8b89eed2c7abcaeda
Sasanda Saumya <sasadasaumya@gmail.com>
2025-05-29 12:46:07 +0530

    create map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, View } from "react-native";
+
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = "Waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+    console.error("Error fetching location:", errorMsg);
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log("Current Location:", location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 6.9271, // Center on Colombo
+          longitude: 79.8612,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        zoomEnabled={true}
+      >
+        <Marker
+          coordinate={{ latitude: 6.9271, longitude: 79.8612 }}
+          title="Colombo"
+          description="Capital of Sri Lanka"
+          onPress={() => alert("Marker Pressed!")}
+        />
+
+        <Marker
+          coordinate={{ latitude: 6.9621, longitude: 79.9076 }}
+          title="Colombo Port City"
+          description="Modern waterfront district"
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +68,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
